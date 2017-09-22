dddc63bf4a90d6df3a85871eb03c383881e9e3e5
rabadan731 <rabadan731@yandex.ru>
2017-09-22 23:38:37 +0300

    helps games:correct help_hash and fifty_fifty

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -37,6 +37,24 @@ RSpec.describe GameQuestion, type: :model do
     it '.correct_answer_key' do
       expect(game_question.correct_answer_key).to eq('b')
     end
+
+    it 'correct .help_hash' do
+      # на фабрике у нас изначально хэш пустой
+      expect(game_question.help_hash).to eq({})
+
+      # добавляем пару ключей
+      game_question.help_hash[:some_key1] = 'blabla1'
+      game_question.help_hash['some_key2'] = 'blabla2'
+
+      # сохраняем модель и ожидаем сохранения хорошего
+      expect(game_question.save).to be_truthy
+
+      # загрузим этот же вопрос из базы для чистоты эксперимента
+      gq = GameQuestion.find(game_question.id)
+
+      # проверяем новые значение хэша
+      expect(gq.help_hash).to eq({some_key1: 'blabla1', 'some_key2' => 'blabla2'})
+    end
   end
 
   # Группа тестов на помощь игроку
@@ -54,6 +72,21 @@ RSpec.describe GameQuestion, type: :model do
       # мы не можем знать распределение, но может проверить хотя бы наличие нужных ключей
       ah = game_question.help_hash[:audience_help]
       expect(ah.keys).to contain_exactly('a', 'b', 'c', 'd')
+    end
+
+    # проверяем работу 50/50
+    it 'correct fifty_fifty' do
+      # сначала убедимся, в подсказках пока нет нужного ключа
+      expect(game_question.help_hash).not_to include(:fifty_fifty)
+      # вызовем подсказку
+      game_question.add_fifty_fifty
+
+      # проверим создание подсказки
+      expect(game_question.help_hash).to include(:fifty_fifty)
+      ff = game_question.help_hash[:fifty_fifty]
+
+      expect(ff).to include('b') # должен остаться правильный вариант
+      expect(ff.size).to eq 2 # всего должно остаться 2 варианта
     end
   end
 end
