32deb280c8446c8f80f8ce7b5e6cd6b61639b374
rabadan731 <rabadan731@yandex.ru>
2017-09-22 21:14:50 +0300

    conntroller spec: takes money

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -60,5 +60,18 @@ RSpec.describe GamesController, type: :controller do
       expect(response).to redirect_to(root_path)
       expect(flash[:alert]).to be
     end
+
+    it 'takes money' do
+      game_w_questions.update_attribute(:current_level, 4)
+
+      put :take_money, id: game_w_questions.id
+      game = assigns(:game)
+      user.reload
+
+      expect(game).to be_finished
+      expect(game.prize).to eq(500)
+      expect(user.balance).to eq(500)
+      expect(response).to redirect_to(user_path(user))
+    end
   end
 end
