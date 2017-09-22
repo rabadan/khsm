381580665a19a7a7485fd50db4eb37edd9115919
rabadan731 <rabadan731@yandex.ru>
2017-09-22 23:45:44 +0300

    helps games spec: correct friend_call

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -88,5 +88,16 @@ RSpec.describe GameQuestion, type: :model do
       expect(ff).to include('b') # должен остаться правильный вариант
       expect(ff.size).to eq 2 # всего должно остаться 2 варианта
     end
+
+    # проверяем работу звонка другу
+    it 'correct friend_call' do
+      # сначала убедимся, в подсказках пока нет нужного ключа
+      expect(game_question.help_hash).not_to include(:fifty_fifty)
+      # вызовем подсказку
+      game_question.add_fifty_fifty
+
+      # проверим создание подсказки
+      expect(game_question.help_hash).to include(:fifty_fifty)
+    end
   end
 end
